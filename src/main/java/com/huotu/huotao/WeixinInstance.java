6cd484d80eb3da313f476152529d1705a13765f8
Cai Jiang <luffy.ja@gmail.com>
2017-02-21 10:53:52 +0800

    调整代码

--- src/main/java/com/huotu/huotao/WeixinInstance.java
+++ src/main/java/com/huotu/huotao/WeixinInstance.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.core.io.Resource;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Optional;
 
@@ -20,8 +20,6 @@ import java.util.Optional;
  */
 public class WeixinInstance {
 
-    private final WebDriver driver = new ChromeDriver();
-
     static {
         try {
             DriverSetup.setup();
@@ -29,6 +27,8 @@ public class WeixinInstance {
             throw new RuntimeException(e);
         }
     }
+
+    private final WebDriver driver = new ChromeDriver();
 
     public WeixinInstance() {
         driver.get("https://wx.qq.com");
@@ -72,37 +72,7 @@ public class WeixinInstance {
     public void sendTextMessage(String to, String text) {
         if (!isLoggedIn())
             login();
-        WebElement main = driver.findElement(By.className("main"));
-
-        WebElement bar = main.findElement(By.id("search_bar"));
-
-        WebElement searchInput = bar.findElement(By.tagName("input"));
-//        contacts scrollbar-dynamic scroll-content scroll-scrolly_visible
-        searchInput.clear();
-        searchInput.sendKeys(to);
-
-        new WebDriverWait(driver, 5).until((Predicate<WebDriver>) input
-                -> input != null && findSearchResult(input).isPresent());
-
-        WebElement listDiv = findSearchResult(driver)
-                .orElseThrow(() -> new IllegalStateException("找不到搜索结果列表"));
-
-        listDiv.findElements(By.className("ng-scope")).stream()
-                .filter(element -> !element.findElements(By.tagName("h4")).isEmpty())
-                .filter(element -> element.findElement(By.tagName("h4")).getText().equals(to))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("找不到好友" + to))
-                .click();
-
-        new WebDriverWait(driver, 5)
-                .until(new Predicate<WebDriver>() {
-                    @Override
-                    public boolean apply(WebDriver input) {
-                        if (input == null)
-                            return false;
-                        return input.findElement(By.cssSelector("div.title")).getText().equals(to);
-                    }
-                });
+        chooseContact(to);
 
         WebElement editArea = driver.findElement(By.id("editArea"));
 
@@ -113,10 +83,68 @@ public class WeixinInstance {
         sendButton.click();
     }
 
+    /**
+     * 选择对话的名称
+     *
+     * @param name 名称
+     */
+    private void chooseContact(String name) {
+        if (currentTitle(name, driver))
+            return;
+        WebElement bar = driver.findElement(By.id("search_bar"));
+
+        WebElement searchInput = bar.findElement(By.tagName("input"));
+//        contacts scrollbar-dynamic scroll-content scroll-scrolly_visible
+        searchInput.clear();
+        searchInput.sendKeys(name);
+
+        new WebDriverWait(driver, 5).until((Predicate<WebDriver>) input
+                -> input != null && findSearchResult(input).isPresent());
+
+        WebElement listDiv = findSearchResult(driver)
+                .orElseThrow(() -> new IllegalStateException("找不到搜索结果列表"));
+
+        listDiv.findElements(By.className("ng-scope")).stream()
+                .filter(element -> !element.findElements(By.tagName("h4")).isEmpty())
+                .filter(element -> element.findElement(By.tagName("h4")).getText().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("找不到好友" + name))
+                .click();
+
+        new WebDriverWait(driver, 5)
+                .until((Predicate<WebDriver>) input
+                        -> input != null && currentTitle(name, input));
+    }
+
+    private boolean currentTitle(String title, WebDriver input) {
+        return input.findElement(By.cssSelector("div.title")).getText().equals(title);
+    }
+
     private Optional<WebElement> findSearchResult(WebDriver driver) {
         return driver.findElements(By.className("contacts")).stream()
 //                .peek(System.out::println)
                 .filter(element -> element.getAttribute("class").contains("scroll-content"))
                 .findFirst();
+    }
+
+    public void sendFile(String to, Resource resource) throws IOException {
+        if (!isLoggedIn())
+            login();
+        chooseContact(to);
+
+        WebElement toolBar = driver.findElement(By.id("tool_bar"));
+        // webuploader-pick
+        WebElement uploader = toolBar.findElements(By.tagName("a")).stream()
+                .peek(System.out::println)
+                .filter(element -> !element.findElements(By.className("webuploader-pick")).isEmpty())
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("找不到上传按钮"));
+//        uploader.click();
+        WebElement fileInput = uploader.findElement(By.cssSelector("input[type=file]"));
+
+        // 保存这个Resource到临时文件
+//        File file = File.createTempFile("",resource.get)
+        fileInput.sendKeys(resource.getFile().getAbsolutePath());
+        System.out.println(1);
     }
 }
--- src/test/java/com/huotu/huotao/WeixinInstanceTest.java
+++ src/test/java/com/huotu/huotao/WeixinInstanceTest.java
@@ -1,6 +1,7 @@
 package com.huotu.huotao;
 
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -16,6 +17,8 @@ public class WeixinInstanceTest {
         WeixinInstance instance = new WeixinInstance();
 //        instance.login();
         instance.sendTextMessage("Guo Childe", "hello world, at " + new Date());
+
+        instance.sendFile("Guo Childe", new ClassPathResource("/girl.jpeg"));
     }
 
 }
